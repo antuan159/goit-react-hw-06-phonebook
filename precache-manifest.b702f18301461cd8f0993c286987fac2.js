73592f57c1f8f5487abbb420b80ebfab36987248
antuan159 <lanos159@ukr.net>
2020-01-07 20:42:39 +0200

    Updates

---
+++ precache-manifest.b702f18301461cd8f0993c286987fac2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2027e2e2e808b23568a46f11b76a31e1",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "2f821f9a646a9beb411b",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.552727f9.chunk.js"
+  },
+  {
+    "revision": "6b1e30fbae570017776d1838e82dbe98",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.552727f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6cca678a0ea1cff39ac7",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.9ac69f5c.chunk.js"
+  },
+  {
+    "revision": "c04d2f38d7f83b71add9",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.c6773e95.js"
+  }
+]);
